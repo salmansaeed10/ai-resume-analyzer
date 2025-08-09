5c4df6ab402119fd3ae1b8baecb10b833347abab
salmansaeed10 <mssaeed27@gmail.com>
2025-08-10 01:54:12 +0500

    Created Auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/navbar";
+import {resumes} from "../../constants";
+import {callback} from "fdir/dist/api/async";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "AI based analysis for your Job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover ">
+      <Navbar />
+
+      <section className="main-section">
+          <div className="page-heading">
+              <h1>Track Your Application and Resume Ratings</h1>
+              <h2>Review your Submissions and Check AI Powered Feedback</h2>
+          </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+              {resumes.map((resume) => (
+                  <ResumeCard key={resume.id} resume={resume} />
+                  ))}
+          </div>
+          )}
+      </section>
+  </main>
 }
